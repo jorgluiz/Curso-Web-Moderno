ade880d61c7a564a1739f4456563e2f8b80ad20e
Jorge Luiz <jorgeluizti@hotmail.com>
2021-10-06 03:35:30 -0300

    'JS_base'

---
+++ 01_Fundamentos/07math.js
@@ -0,0 +1,5 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2) //raio elevado ao quadrado
+
+console.log(Math.round(area))
+console.log(typeof Math) 
---
+++ 01_Fundamentos/40ternario_EX.js
@@ -0,0 +1,10 @@
+var nome = 'Jorge Luiz', idade = 29
+
+idade >= 18 ? ( ("OK, você pode entrar na festa."),
+              console.log(nome + ", entrou!" + ' Pois sua idade é = ' + idade)
+) : 
+
+(("Desculpe, você não pode entrar na festa!"),
+    console.log(nome + " não entrou!")
+)
+
