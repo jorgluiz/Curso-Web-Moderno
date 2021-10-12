e05995a3127105c36cb005b7b4f3ef2706d8d34f
Jorge Luiz <jorgeluizti@hotmail.com>
2021-10-12 02:52:35 -0300

    corrigindo a implementação reduce

--- 05_Array/14_reduce3.js
+++ 05_Array/14_reduce3.js
@@ -17,10 +17,10 @@
 
 
 Array.prototype.reduce2 = function (callback, valorInicial) {
-    const valorInidical = valorInicial ? 0 : 1
+    const indiceInicial = valorInicial ? 0 : 1
     let acumulador = valorInicial || this[0]
 
-    for (let i = 1; i < this.length; i++) {
+    for (let i = indiceInicial; i < this.length; i++) {
         acumulador = callback(acumulador, this[i], i, this)
     }
     return acumulador
