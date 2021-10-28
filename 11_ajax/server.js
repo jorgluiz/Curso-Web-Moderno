da35f0e113218e03d84c58a3862671f7d2153cdf
Jorge Luiz <jorgeluizti@hotmail.com>
2021-10-27 21:10:43 -0300

    config server multer

--- 11_ajax/server.js
+++ 11_ajax/server.js
@@ -6,5 +6,33 @@ app.use(express.static('.'))  //vai servi o conteúdo static
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// interpretando o arquivo 'upload' (BackEnd)
+const multer = require('multer') // import 'multer'
+
+const storage = multer.diskStorage({ //config parte de armazenamento
+    destination: function (req, file, callback) {
+        callback(null, './upload')//pasta de destino
+    },
+    filename: function (req, file, callback) { //nome do arquivo que será enviado para pasta './upload'
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => { //requisição tipo post
+    upload(req, res, err => {
+        if (err){
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //app.get('/teste', (req, res) => res.send(new Date))  //vai servi p/página (data atual)
 app.listen(8080, () => console.log('Executando...'))
